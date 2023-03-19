f3ccff11ef6ae21f7cfbcef184c54d78829522a8
Ilia Choly <ilia.choly@gmail.com>
2023-03-19 11:13:53 -0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/icholy/codegpt
+
+go 1.20
+
+require github.com/sashabaranov/go-openai v1.5.1
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func main() {
+	var temp, topP float64
+	flag.Float64Var(&temp, "temp", 0, "temperature")
+	flag.Float64Var(&topP, "top_p", 0, "TopP")
+	flag.Parse()
+	// Fetch the OpenAI API key from the environment
+	key := os.Getenv("OPENAI_API_KEY")
+	if key == "" {
+		log.Fatal("OPENAI_API_KEY environment variable not set")
+	}
+	// read stdin
+	input, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// make request
+	client := openai.NewClient(key)
+	model := openai.CodexCodeDavinci002
+	edits, err := client.Edits(
+		context.Background(),
+		openai.EditsRequest{
+			Model:       &model,
+			Input:       string(input),
+			Instruction: strings.Join(flag.Args(), " "),
+			N:           1,
+			Temperature: float32(temp),
+			TopP:        float32(topP),
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(edits.Choices) == 0 {
+		log.Fatal("no responses")
+	}
+	for _, c := range edits.Choices {
+		fmt.Print(c.Text)
+		break
+	}
+}
