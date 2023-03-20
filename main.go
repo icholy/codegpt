549c5a693a531753ee990ae9247cda248ed1bd42
Ilia Choly <ilia.choly@gmail.com>
2023-03-19 22:07:36 -0400

    allow reading/writing to file

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -13,54 +14,88 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
-func main() {
-	var force, raw bool
-	var max int
-	var temp, topP float64
-	var filename string
+var (
+	force, raw, write bool
+	max               int
+	temp, topP        float64
+	ifile             string
+	sfile             string
+)
+
+func init() {
 	flag.BoolVar(&raw, "r", false, "raw output")
 	flag.BoolVar(&force, "f", false, "force")
+	flag.BoolVar(&write, "w", false, "write")
 	flag.Float64Var(&temp, "t", 0.6, "temperature")
 	flag.Float64Var(&topP, "p", 1, "TopP")
 	flag.IntVar(&max, "max", 4000, "max tokens")
-	flag.StringVar(&filename, "i", "", "instructions file")
+	flag.StringVar(&ifile, "i", "", "instruction file")
+	flag.StringVar(&sfile, "s", "", "source file")
 	flag.Parse()
+}
+
+func main() {
 	// instructions
-	var prompt []string
-	if filename != "" {
-		data, err := os.ReadFile(filename)
+	var instructions []string
+	if ifile != "" {
+		data, err := os.ReadFile(ifile)
 		if err != nil {
 			log.Fatal(err)
 		}
-		prompt = append(prompt, string(data))
+		instructions = append(instructions, string(data))
 	}
-	prompt = append(prompt, strings.Join(flag.Args(), " "))
-	if len(prompt) == 0 {
+	instructions = append(instructions, strings.Join(flag.Args(), " "))
+	if len(instructions) == 0 {
 		log.Fatal("no instructions")
 	}
+	var code string
+
+	if sfile != "" {
+		data, err := os.ReadFile(sfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		code = string(data)
+	} else {
+		stat, _ := os.Stdin.Stat()
+		if stat.Mode()&os.ModeCharDevice == 0 {
+			data, err := io.ReadAll(os.Stdin)
+			if err != nil {
+				log.Fatal(err)
+			}
+			code = string(data)
+		}
+	}
+	output, err := EditCode(strings.Join(instructions, " "), code)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if sfile != "" && write {
+		if err := os.WriteFile(sfile, []byte(output), os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		fmt.Print(output)
+	}
+}
+
+func EditCode(instructions string, code string) (string, error) {
 	// Fetch the OpenAI API key from the environment
 	key := os.Getenv("OPENAI_API_KEY")
 	if key == "" {
 		log.Fatal("OPENAI_API_KEY environment variable not set")
 	}
-	// read stdin
-	prompt = append(prompt, "```")
-	stat, _ := os.Stdin.Stat()
-	if stat.Mode()&os.ModeCharDevice == 0 {
-		code, err := io.ReadAll(os.Stdin)
-		if err != nil {
-			log.Fatal(err)
-		}
-		prompt = append(prompt, string(code))
-		// make sure we have enough tokens to at least replace the previous code
-		if tok := EstimateTokens(len(code)); tok > max {
-			log.Fatalf("-max isn't large enough for the provided code: Len=%d EstimatedTokens=%d CodeLen=%d", len(code), tok, max)
-		}
-	}
-	prompt = append(prompt, "```")
+	// create prompt
+	var prompt []string
+	// add instructions to prompt
+	prompt = append(prompt, instructions)
+	prompt = append(prompt, "```", code, "```")
 	// sanity check the tokens
+	if tok := EstimateTokens(len(code)); tok > max {
+		log.Fatalf("MaxTokens isn't large enough for the provided code: Len=%d EstimatedTokens=%d MaxTokens=%d", len(code), tok, max)
+	}
 	if n := EstimateTokens(len(strings.Join(prompt, " "))) + max; n > 8192 && !force {
-		log.Fatal("-tokens + EstimatedTokens(len(prompt)) exceeds the model's limit of 8192")
+		return "", errors.New("MaxTokens + EstimatedTokens(len(Prompt)) exceeds the model's limit of 8192")
 	}
 	// make request
 	client := openai.NewClient(key)
@@ -92,19 +127,16 @@ func main() {
 		},
 	)
 	if err != nil {
-		log.Fatal(err)
-	}
-	if len(completions.Choices) == 0 {
-		log.Fatal("no responses")
+		return "", err
 	}
 	for _, c := range completions.Choices {
 		if raw {
-			fmt.Print(c.Message.Content)
+			return c.Message.Content, nil
 		} else {
-			fmt.Print(ExtractCode(c.Message.Content))
+			return ExtractCode(c.Message.Content), nil
 		}
-		break
 	}
+	return "", errors.New("no responses")
 }
 
 func EstimateTokens(bytes int) int {
