1284169387088a34e78f60ea4d009073f4fb9481
Ilia Choly <ilia.choly@gmail.com>
2023-03-19 20:30:21 -0400

    switch to use gpt-4

--- main.go
+++ main.go
@@ -13,23 +13,25 @@ import (
 )
 
 func main() {
+	var maxtokens int
 	var temp, topP float64
 	var filename string
 	flag.Float64Var(&temp, "temp", 0, "temperature")
 	flag.Float64Var(&topP, "top_p", 1, "TopP")
+	flag.IntVar(&maxtokens, "tokens", 8000, "max tokens")
 	flag.StringVar(&filename, "i", "", "instructions file")
 	flag.Parse()
 	// instructions
-	var instructions []string
+	var prompt []string
 	if filename != "" {
 		data, err := os.ReadFile(filename)
 		if err != nil {
 			log.Fatal(err)
 		}
-		instructions = append(instructions, string(data))
+		prompt = append(prompt, string(data))
 	}
-	instructions = append(instructions, flag.Args()...)
-	if len(instructions) == 0 {
+	prompt = append(prompt, strings.Join(flag.Args(), " "))
+	if len(prompt) == 0 {
 		log.Fatal("no instructions")
 	}
 	// Fetch the OpenAI API key from the environment
@@ -38,37 +40,53 @@ func main() {
 		log.Fatal("OPENAI_API_KEY environment variable not set")
 	}
 	// read stdin
-	var input string
+	prompt = append(prompt, "```")
 	stat, _ := os.Stdin.Stat()
 	if stat.Mode()&os.ModeCharDevice == 0 {
-		data, err := io.ReadAll(os.Stdin)
+		code, err := io.ReadAll(os.Stdin)
 		if err != nil {
 			log.Fatal(err)
 		}
-		input = string(data)
+		prompt = append(prompt, string(code))
 	}
+	prompt = append(prompt, "```")
 	// make request
 	client := openai.NewClient(key)
-	model := "code-davinci-edit-001"
-	edits, err := client.Edits(
+	completions, err := client.CreateChatCompletion(
 		context.Background(),
-		openai.EditsRequest{
-			Model:       &model,
-			Input:       input,
-			Instruction: strings.Join(instructions, " "),
-			N:           1,
+		openai.ChatCompletionRequest{
+			Model: "gpt-4",
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role: openai.ChatMessageRoleSystem,
+					Content: strings.Join(
+						[]string{
+							"You are a programming assistant.",
+							"All requests be formatted with a set of instructions followed by a fenced code block.",
+							"You will apply the instructions to the code block and output the modified code without a code block.",
+						},
+						" ",
+					),
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: strings.Join(prompt, "\n"),
+				},
+			},
+			MaxTokens:   maxtokens,
 			Temperature: float32(temp),
 			TopP:        float32(topP),
+			N:           1,
 		},
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if len(edits.Choices) == 0 {
+	if len(completions.Choices) == 0 {
 		log.Fatal("no responses")
 	}
-	for _, c := range edits.Choices {
-		fmt.Print(c.Text)
+	for _, c := range completions.Choices {
+		fmt.Print(c.Message)
 		break
 	}
 }
